1e0ba036aa031225539bd0d5e4f658bca1e982a9
Matthias Engels <matthias.engels@gmail.com>
2020-02-09 20:32:57 +0000

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -7,13 +7,13 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS"
+staging_songs_table_drop = "DROP TABLE IF EXISTS"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -23,19 +23,60 @@ staging_events_table_create= ("""
 staging_songs_table_create = ("""
 """)
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE songplays (
+    songplay_id IDENTITY(0,1),
+    start_time timestamp,
+    user_id int,
+    level varchar,
+    song_id varchar,
+    artist_id varchar,
+    session_id int,
+    location varchar,
+    user_agent varchar,
+    PRIMARY KEY ()
+)
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE user (
+    user_id int,
+    fisrt_name varchar,
+    last_name varchar,
+    gender varchar,
+    level varchar,
+    PRIMARY KEY (user_id)
+    );
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE songs (
+    song_id varchar,
+    title varchar,
+    artist_id varchar,
+    year int,
+    duration int,
+    PRIMARY KEY (song_id)
+);
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE artist (
+    artist_id int,
+    name varchar,
+    location varchar,
+    latitude numeric,
+    longitude numeric,
+    PRIMARY KEY (artist_id)
+)
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE time (
+    start_time timestamp,
+    hour numeric,
+    day numeric,
+    week numeric,
+    month numeric,
+    year numeric,
+    weekday varchar,
+    PRIMARY KEY (start_time)
+)
 """)
 
 # STAGING TABLES
